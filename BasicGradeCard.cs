b9b9595ef1fa10879abf19dc4c93ffa805009be9
unknown <vasanthgamer2000@gmail.com>
2023-08-01 16:54:35 +0530

    Changes have been committed

---
+++ BasicGradeCard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section5._10
+{
+    public class BasicGradeCard
+    {
+        protected int totalMarks;
+
+        public BasicGradeCard()
+        {
+            totalMarks = 0;
+        }
+
+        public virtual int GetTotalMarks()
+        {
+            return totalMarks;
+        }
+
+    }
+}
---
+++ MiddleSchoolGradeCard.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section5._10
+{
+    public class MiddleSchoolGradeCard: BasicGradeCard
+    {
+        private int maths = 0;
+        private int english = 0;
+        private int secondLanguage = 0;
+        private int geography = 0;
+        private int history = 0;
+
+        public int Maths
+        {
+            get { return maths; }
+            set { maths = value; }
+        }
+
+        public int English
+        {
+            get { return english; }
+            set { english = value; }
+        }
+
+        public int SecondLanguage
+        {
+            get { return secondLanguage; }
+            set { secondLanguage = value; }
+        }
+
+        public int Geography
+        {
+            get { return geography; }
+            set { geography = value; }
+        }
+
+        public int History
+        {
+            get { return history; }
+            set { history = value; }
+        }
+
+        public override int GetTotalMarks()
+        {
+            this.totalMarks = maths + english + geography + history;
+            return totalMarks;
+        }
+
+    }
+}
